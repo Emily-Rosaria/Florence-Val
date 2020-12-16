ef14a2f12ed13d87597c148e7d044da51faf60dc
Emily Rose <ebonywoodhellscythe@gmail.com>
2020-12-16 19:01:20 +0000

    Minor help-text changes.

--- commands/tavern/resetwords.js
+++ commands/tavern/resetwords.js
@@ -3,11 +3,11 @@ const Users = require("./../../database/models/users.js"); // users model
 const Discord = require('discord.js'); // Image embed
 
 module.exports = {
-  name: 'clearquests', // The name of the command
-  aliases: ['resetquests'],
-  description: 'Clears a user\'s stored word/character counts for their quests. Does not reset overall word/char counts.', // The description of the command (for help text)
+  name: 'clearwords', // The name of the command
+  aliases: ['resetwords'],
+  description: 'Clears a user\'s total stored word/character counts on the discord.', // The description of the command (for help text)
   args: true, // Specified that this command doesn't need any data other than the command
-  perms: "mod",
+  perms: "admin",
   allowDM: false,
   cooldown: 3,
   usage: '<user>', // Help text to explain how to use the command (if it had any arguments)
@@ -33,7 +33,7 @@ module.exports = {
     if (!userData) {
       return message.reply("The user `"+user.tag+"` had no data stored by the bot.");
     } else {
-      return message.reply("The user `"+user.tag+" previously had the following word/character counts before they were reset to 0.\n> Words: "+userData.totalWords+"\n> Characters: "+userData.totalChars);
+      return message.reply("The user `"+user.tag+"` previously had the following word/character counts before they were reset to 0.\n> Words: "+userData.totalWords+"\n> Characters: "+userData.totalChars);
     }
   },
 };
