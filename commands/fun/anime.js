89b1b297a2b3466e2aa1a37c277900838cf01f72
Emily Rose <ebonywoodhellscythe@gmail.com>
2021-03-27 23:59:04 +0000

    Anime tag fix

--- commands/fun/anime.js
+++ commands/fun/anime.js
@@ -10,20 +10,24 @@ module.exports = {
     description: 'Get random anime pics from safebooru!', // The description of the command (for help text)
     args: false, // Specified that this command doesn't need any data other than the command
     allowDM: true,
-    perms: 'writer', //restricts to users with the "writer" role noted at config.json
+    perms: 'verified', //restricts to users with the "verifed" role noted at config.json
     usage: '[image-tag 1] [image-tag 2] [...]', // Help text to explain how to use the command (if it had any arguments)
-    async execute(message, args) {
+    execute(message, args) {
         // Get image from the api.
-        let tags = args.length > 0 ? args : ['all'];
+        let tags = args.length > 0 ? args : [];
         if (tags.length > 3) {
           tags = tags.slice(0,3);
         }
-        const image = await Booru.search('safebooru', tags, { limit: 1, random: true });
-        const embed = new Discord.MessageEmbed()
-        .setColor('#2e51a2')
-        .setImage(image[0].fileUrl)
-        .setFooter('Image from safebooru')
-        .setTimestamp()
-        message.reply(embed); // Replies to the user with a random image
+        Booru.search('safebooru', tags, { limit: 1, random: true }).then(image =>{
+          const embed = new Discord.MessageEmbed()
+          .setColor('#2e51a2')
+          .setImage(image[0].fileUrl)
+          .setFooter('Image from safebooru')
+          .setTimestamp()
+          return message.reply(embed);
+        })
+        .catch(error => {
+          message.reply('Unable to fetch an image. Try again in a few minutes.');
+        });
     },
 };
